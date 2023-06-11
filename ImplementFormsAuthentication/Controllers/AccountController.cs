6baabbb814af4fefe52cc16fda4b316fda67c2e7
shekhar <shekhartarare@gmail.com>
2023-06-11 17:02:09 +0530

    Add project files.

---
+++ ImplementFormsAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ImplementFormsAuthentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (IsValidUser(username, password))
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                return RedirectToAction("Index", "Home"); // Redirect to default landing page
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+        private bool IsValidUser(string username, string password)
+        {
+            return FormsAuthentication.Authenticate(username, password);
+        }
+    }
+}
